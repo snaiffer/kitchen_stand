5ebccea5717c8ba2dbd6963902138c74589a3cb5
Alexander Danilov <snaiffer@gmail.com>
2015-06-09 16:11:58 +0400

    fixed autobackup; added autobackup_sonda

--- data/autobackup_db/init.sh
+++ data/autobackup_db/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dbname="$1"
+dbname_sonda="$2"
 
 printf "Setting /etc/crontab... "
 grep -iRnH "0 0 \* \* \* root.*/etc/cron.daily$" /etc/crontab &> /dev/null
@@ -13,8 +14,13 @@ echo "done."
 printf "Setting autobackup... "
 mkdir -p /etc/cron.daily
 cp -f autobackup /etc/cron.daily/
-if [[ "$dbname" != "" ]]; then 
+if [[ "$dbname" != "" ]]; then
   sed -i "s/kitchen/$dbname/g" /etc/cron.daily/autobackup
 fi
+cp -f autobackup_sonda /etc/cron.daily/
+if [[ "$dbname_sonda" != "" ]]; then
+  sed -i "s/mainDB/$dbname_sonda/g" /etc/cron.daily/autobackup_sonda
+fi
+chmod +x /etc/cron.daily/*
 echo "done."
 echo
